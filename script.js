73caa846d17eebec4dcbb966556a6341c8832818
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-05-31 16:14:30 -0500

    Update script.js
    Display numbers function

--- script.js
+++ script.js
@@ -2,33 +2,38 @@ let history = [];
 let currentIndex = -1;
 
 function generatePowerballNumbers() {
-    let whiteBalls = new Set();
-    while (whiteBalls.size < 5) {
-        whiteBalls.add(Math.floor(Math.random() * 69) + 1);
-    }
-    let powerball = Math.floor(Math.random() * 26) + 1;
-    return {
-        whiteBalls: [...whiteBalls].sort((a, b) => a - b),
-        powerball: powerball,
-    };
+  let whiteBalls = new Set();
+  while (whiteBalls.size < 5) {
+    whiteBalls.add(Math.floor(Math.random() * 69) + 1);
+  }
+  let powerball = Math.floor(Math.random() * 26) + 1;
+  return {
+    whiteBalls: [...whiteBalls].sort((a, b) => a - b),
+    powerball: powerball,
+  };
 }
 
 document.getElementById("generate-btn").addEventListener("click", function () {
-    const numbers = generatePowerballNumbers();
-    history.push(numbers);
-    currentIndex++;
-    displayNumbers(numbers);
+  const numbers = generatePowerballNumbers();
+  history.push(numbers);
+  currentIndex++;
+  displayNumbers(numbers);
 });
 
 document.getElementById("prev-btn").addEventListener("click", function () {
-    if (currentIndex > 0) {
-        currentIndex--;
-        const numbers = history[currentIndex];
-        displayNumbers(numbers);
-    }
+  if (currentIndex > 0) {
+    currentIndex--;
+    const numbers = history[currentIndex];
+    displayNumbers(numbers);
+  }
 });
 
 function displayNumbers(numbers) {
-    document.getElementById("white-balls").textContent = numbers.whiteBalls.join(", ");
-    document.getElementById("powerball-number").textContent = numbers.powerball;
+  const whiteBallsElement = document.getElementById("white-balls");
+  whiteBallsElement.textContent = numbers.whiteBalls.join(", ");
+  whiteBallsElement.style.border = "3px solid black";
+
+  const powerballElement = document.getElementById("powerball");
+  powerballElement.textContent = numbers.powerball;
+  powerballElement.style.border = "3px solid red";
 }
