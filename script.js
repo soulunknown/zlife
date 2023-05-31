55a159c099a8f2a136bb43e5e196021faef1e092
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-05-31 15:54:37 -0500

    Update script.js
    Previous button

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+let history = [];
+let currentIndex = -1;
+
 function generatePowerballNumbers() {
     let whiteBalls = new Set();
     while (whiteBalls.size < 5) {
-      whiteBalls.add(Math.floor(Math.random() * 69) + 1);
+        whiteBalls.add(Math.floor(Math.random() * 69) + 1);
     }
-  
     let powerball = Math.floor(Math.random() * 26) + 1;
-  
     return {
-      whiteBalls: [...whiteBalls].sort((a, b) => a - b),
-      powerball: powerball,
+        whiteBalls: [...whiteBalls].sort((a, b) => a - b),
+        powerball: powerball,
     };
-  }
-  // script.js
+}
+
 document.getElementById("generate-btn").addEventListener("click", function () {
     const numbers = generatePowerballNumbers();
+    history.push(numbers);
+    currentIndex++;
+    displayNumbers(numbers);
+});
+
+document.getElementById("prev-btn").addEventListener("click", function () {
+    if (currentIndex > 0) {
+        currentIndex--;
+        const numbers = history[currentIndex];
+        displayNumbers(numbers);
+    }
+});
+
+function displayNumbers(numbers) {
     document.getElementById("white-balls").textContent = numbers.whiteBalls.join(", ");
-    document.getElementById("powerball").textContent = "Powerball: " + numbers.powerball;
-  });
-  
+    document.getElementById("powerball-number").textContent = numbers.powerball;
+}
